4ab42711b9b952ab61c58cc2697c6e29ddcd2624
coldoem <thomasdewit111@gmail.com>
2020-08-09 14:09:15 +0200

    added basic transaction vars + getters/setters

---
+++ classes/Transaction.php
@@ -0,0 +1,45 @@
+<?php
+    class Transaction{
+        private $fromUser;
+        private $toUser;
+        private $amount;
+        private $date;
+        private $details;
+
+        public function getFromUser(){
+            return $this->fromUser;
+        }
+        public function getToUser(){
+            return $this->toUser;
+        }
+        public function getAmount(){
+            return $this->amount;
+        }
+        public function getDate(){
+            return $this->date;
+        }
+        public function getDetails(){
+            return $this->details;
+        }
+        public function setFromUser($fromUser){
+            $this->fromUser = $fromUser;
+            return $this;
+        }
+        public function setToUser($toUser){
+            $this->toUser = $toUser;
+            return $this;
+        }
+        public function setAmount($amount){
+            $this->amount = $amount;
+            return $this;
+        }
+        public function setDate($date){
+            $this->date = $date;
+            return $this;
+        }
+        public function setDetails($details){
+            $this->details = $details;
+            return $this;
+        }
+    }
+?>
