ebc56ae52ae5b370c668199b3ea7351b39fc6980
coldoem <thomasdewit111@gmail.com>
2020-08-09 16:14:41 +0200

    fixed css

--- login.php
+++ login.php
@@ -10,12 +10,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" ref="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Log In</title>
 </head>
 <body>
     <div class="nav">
-    
+        
     </div>
     <div class="main">
     
