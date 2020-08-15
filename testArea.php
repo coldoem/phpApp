f85c2d677baf6b736de32d18e6cec68b1f5a2782
coldoem <thomasdewit111@gmail.com>
2020-08-15 17:28:30 +0200

    test area done

--- testArea.php
+++ testArea.php
@@ -1,6 +1,21 @@
 <?php
     if(!empty($_POST)){
-
+        if(isset($_POST["dummyTransaction"])){
+            //ask for details?
+            //create dummy transaction
+        }
+        if(isset($_POST["dummyUser"])){
+            //ask for details?
+            //create dummy user
+        }
+        if(isset($_POST["addSaldo"])){
+            //ask for amount
+            //add saldo
+        }
+        if(isset($_POST["removeSaldo"])){
+            //ask for amount
+            //remove saldo
+        }
     }
 ?>
 <!DOCTYPE html>
@@ -13,16 +28,16 @@
 <body>
     <form action="" method="post">
         <label for="dummyTransaction">Create Dummy Transaction:</label>
-        <input type="checkbox" name="dummyTransaction" value="createDummyTransaction">
+        <input type="checkbox" name="dummyTransaction" value="1">
         <br>
         <label for="dummyTransaction">Create Dummy User:</label>
-        <input type="checkbox" name="dummyUser" value="createDummyUser">
+        <input type="checkbox" name="dummyUser" value="1">
         <br>
         <label for="dummyTransaction">Add Saldo:</label>
-        <input type="checkbox" name="addSaldo" value="addSaldo">
+        <input type="checkbox" name="addSaldo" value="1">
         <br>
         <label for="dummyTransaction">Remove Saldo:</label>
-        <input type="checkbox" name="removeSaldo" value="removeSaldo">
+        <input type="checkbox" name="removeSaldo" value="1">
         <br>
         <input type="submit" value="Execute">
     </form>
