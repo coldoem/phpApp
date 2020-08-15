228c546eca660affa61f406954ddcc1f2fb60170
coldoem <thomasdewit111@gmail.com>
2020-08-15 22:06:58 +0200

    added extra stylesheet because of caching issues

--- index.php
+++ index.php
@@ -13,27 +13,33 @@
 
     $recentTransactions = $user->getRecentTransactions();
 
-    if(!empty($_POST["amount"])){
-        if(!empty($_POST["details"])){
+    if(!empty($_POST)){
 
+    }
+
+    /*if(!empty($_POST["amount"])){
+        if(!empty($_POST["details"])){
+            $fromUser = $user->getEmail();
+            $recipient = new User();
+            
         }else{
             $detailsMessage = true;
         }
-    }
+    }*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/style2.css">
         <title>Home</title>
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/jquery.session@1.0.0/jquery.session.min.js"></script>
     </head>
     <body>
         <div class="logout">
-            <a href="logout.php"><button>Log Out</button></a>
+            <a href="logout.php">Log Out</a>
         </div>
         <div class="top">
             <!-- current saldo -->
@@ -49,7 +55,7 @@
                 <input type="text" id="searchBar" name="searchBar" placeholder="example person">
                 <input type="submit" value="Search">
             </form>
-            <?php if(!empty($_POST)) : ?>
+            <?php if(!empty($_POST) && $validTarget) : ?>
                 <form action="" method="post">
                     <label for="amount">Amount:</label>
                     <input type="number" min="1" name="amount" value="1">
